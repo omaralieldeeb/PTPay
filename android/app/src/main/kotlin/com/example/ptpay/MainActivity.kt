c05f733a2b764768445e375cc162a922e90595b6
omaralieldeeb <omar975135@gmail,com>
2025-01-27 11:47:06 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/ptpay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ptpay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+main() => runApp(const PTPayApp());
+class PTPayApp extends StatelessWidget {
+  const PTPayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: null,
+    );
+  }
+}
